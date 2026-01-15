d5af7b7b642e008a5d03ed19bbd1c309013aafc9
Mohammad Asrar Magray <mohammadasrar311@gmail.com>
2026-01-15 09:44:47 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter the number:");
+    scanf("%d", &number);
+    if(number%2 == 0)
+    printf(" even number baby");
+    else{
+        printf("odd number baby");
+    }
+
+}
